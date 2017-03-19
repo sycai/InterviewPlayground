20091403237806bb8bfde5b3bcd558271be85b23
Shenyang Cai <sycai@outlook.com>
2017-03-19 19:57:01 -0400

    Update PartsSum algorithm

--- Trees/PartsSum.java
+++ Trees/PartsSum.java
@@ -1,5 +1,6 @@
 package Trees;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -21,26 +22,58 @@ public class PartsSum {
         return searchPath(root, new HashSet<>(), sum);
     }
 
-    private static int searchPath(BTNode root, HashSet<WrapInt> passValues, int sum) {
+    private static int searchPath(BTNode root, HashSet<WrapInt> remainValues, int sum) {
         if (root == null)   return 0;
         int count = 0;
         WrapInt newElement = new WrapInt(sum);
-        passValues.add(newElement);
-        for (WrapInt r : passValues) {
+        // a remaining value v_i equals to (targetSum - sum on the path from ancestor i)
+        // remainValues is the set of all v_i where i is any ancestor of the current node.
+        remainValues.add(newElement);
+        for (WrapInt r : remainValues) {
             if (root.val == r.val)  count += 1;
         }
 
-        for (WrapInt r : passValues) {
+        for (WrapInt r : remainValues) {
             r.val -= root.val;
         }
-        count += searchPath(root.left, passValues, sum);
-        count += searchPath(root.right, passValues, sum);
+        count += searchPath(root.left, remainValues, sum);
+        count += searchPath(root.right, remainValues, sum);
         // Restore the set to its original state
-        for (WrapInt r : passValues) {
+        for (WrapInt r : remainValues) {
             r.val += root.val;
         }
-        passValues.remove(newElement);
+        remainValues.remove(newElement);
         return count;
+    }
+
+    // Second version uses a hash map. This algorithm is given in the book CCI
+    public static int countPathWithSum(BTNode root, int targetSum) {
+        return countPathWithSum(root, targetSum, 0, new HashMap<>());
+    }
+
+    private static int countPathWithSum(BTNode node, int targetSum, int runningSum, HashMap<Integer, Integer> pathCount) {
+        if (node == null)   return 0; // Base case;
+
+        // Count paths with sum ending at the current node
+        runningSum += node.val;
+        int sum = runningSum - targetSum;
+        int totalPaths = pathCount.getOrDefault(sum, 0);
+
+        // If runningSum equals targetSum, then one additional path starts at root. Add this path
+        if (runningSum == targetSum)    totalPaths++;
+
+        // Increment pathCount, recurse, then decrement pathCount
+        incrementHashTable(pathCount, runningSum, 1);
+        totalPaths += countPathWithSum(node.left, targetSum, runningSum, pathCount);
+        totalPaths += countPathWithSum(node.right, targetSum, runningSum, pathCount);
+        incrementHashTable(pathCount, runningSum, -1);
+        return totalPaths;
+    }
+
+    private static void incrementHashTable(HashMap<Integer, Integer> hashTable, int key, int delta) {
+        int newCount = hashTable.getOrDefault(key, 0) + delta;
+        if (newCount == 0)  hashTable.remove(key);
+        else                hashTable.put(key, newCount);
     }
 
     public static void main(String[] args) {
@@ -48,13 +81,16 @@ public class PartsSum {
         n1.left = new BTNode(2);
         n1.right = new BTNode(1);
         n1.left.left = new BTNode(2);
-        BTNode n2 = new BTNode(2);
+        BTNode n2 = new BTNode(-2);
         n2.left = new BTNode(2);
-        n2.right = new BTNode(1);
+        n2.right = new BTNode(-1);
         n2.right.right = new BTNode(2);
         BTNode root = new BTNode(2);
         root.left = n1;
         root.right = n2;
-        System.out.println(numPath(root, 2));
+
+        for (int targetSum = -5; targetSum < 10; targetSum++) {
+            System.out.println(targetSum + ": " + numPath(root, targetSum) + " | " + countPathWithSum(root, targetSum));
+        }
     }
 }
