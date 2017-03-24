e175f5a80fd0b71d2fb4f647abd280d756d39148
Shenyang Cai <sycai@outlook.com>
2017-03-24 19:20:17 -0400

    Add minimun edit distance algorithm

---
+++ DynamicProgramming/MinEditDistance.java
@@ -0,0 +1,62 @@
+package DynamicProgramming;
+
+/**
+ * Created by sycai on 3/24/2017.
+ * Find the shortest edit distance between two strings.
+ */
+public class MinEditDistance {
+    public int answer;
+    public MinEditDistance(String s1, String s2) {
+        int[][] E = new int[s1.length()+1][s2.length()+1];
+
+        // Initialization
+        for (int i = 0; i <= s1.length(); i++) {
+            E[i][0] = i;
+        }
+
+        for (int j = 0; j <= s2.length(); j++) {
+            E[0][j] = j;
+        }
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                char c1 = s1.charAt(i-1), c2 = s2.charAt(j-1);
+                E[i][j] = minOfThree(E[i-1][j] + 1, E[i][j-1] + 1, E[i-1][j-1] + diff(c1, c2));
+            }
+        }
+        answer = E[s1.length()][s2.length()];
+
+        // Printing the table
+        System.out.print("\t\t");
+        for (char c : s2.toCharArray()) {
+            System.out.print(c + "\t");
+        }
+        System.out.println();
+        for (int i = 0; i <= s1.length(); i++) {
+            if (i == 0)  System.out.print("\t");
+            else         System.out.print(s1.charAt(i-1)+ "\t" );
+            for (int j = 0; j <= s2.length(); j++) {
+                System.out.print(E[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private int diff(char c1, char c2) {
+        if (c1 == c2)   return 0;
+        else            return 1;
+    }
+
+    private int minOfThree(int a, int b, int c) {
+        int min = a;
+        if (b < min)  min = b;
+        if (c < min)  min = c;
+        return min;
+    }
+
+    public static void main(String[] args) {
+        MinEditDistance med = new MinEditDistance("EXPONENTIAL", "POLYNOMIAL");
+        System.out.println("The minimum edit distance is: " + med.answer);
+    }
+
+}
--- README.md
+++ README.md
@@ -3,4 +3,5 @@ This repository is used to keep track of my algorithm practice.
 
 Most of the questions come from these two books:
   - *Algorithms (4th edition)* by *Robert Sedgewick and Kevin Wayne*
+  - *Algorithms (1th edition)* by *Sanjoy Dasgupta et al.*
   - *Cracking the Coding Interview (6th edition)* by *Gayle L. McDowell*
