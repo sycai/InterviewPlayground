6ba28b30d2ddee11d1a3f39089e49362d3025301
Shenyang Cai <sycai@outlook.com>
2018-01-26 22:42:59 -0500

    Added true KMP algo

--- Strings/KMP.java
+++ Strings/KMP.java
@@ -1,54 +1,46 @@
 package Strings;
-/**
- * Knuth-Morris-Pratt substring search explained by DFA simulation.
- *
- * Suppose we have pattern "pat" = "ABABAC" and text "txt".
- *
- * There are 7 states for "pat", represented by integers 0 - 6, where 0
- * stands for the start and 6 stands for the end. A path that leads to
- * the "match" transition is depicted as follows:
- *
- *     A        B        A        B        A       C
- * 0  --->  1  --->  2  --->  3  --->  4  ---> 5  ---> 6
- *
- * A two-dimentional array "dfs" can fully represent such automaton:
- *
- * If we are at state S and the first unmatched character from text is C, the next
- * state is dfs[C][S]
- */
-public class KMP {
-    private String pat;
-    // "Deterministic Finite-state Automaton"
-    private int[][] dfa;
 
-    public KMP(String pat) {
-        // Build DFA from pattern
-        this.pat = pat;
-        int M = pat.length();
-        int R = 256;
-        dfa = new int[R][M];
-        // First match makes dfa jumps to the next state
-        dfa[pat.charAt(0)][0] = 1;
-        for (int X = 0, j = 1; j < M; j++) {
-            // Compute dfs[][j]
-            for (int c = 0; c < R; c++) {
-                dfa[c][j] = dfa[c][X];
+/**
+ * Note: Strings are indexed from 1 according to CLRS.
+ * Please read CLRS for detailed mathematical proof of this algorithm.
+ */
+
+public class KMP {
+    private String pattern;
+    private int[] table;
+
+    public KMP(String pattern) {
+        // Compute prefix function
+        this.pattern = pattern;
+        int len = pattern.length();
+        table = new int[len + 1];
+        int k = 0;
+        for (int q = 1; q < len; q++) {
+            while (k > 0 && pattern.charAt(k) != pattern.charAt(q)) {
+                k = table[k-1];
             }
-            // A character match makes dfa jumps to the next state
-            dfa[pat.charAt(j)][j] = j+1;
-            X = dfa[pat.charAt(j)][X];
+            if (pattern.charAt(k) == pattern.charAt(q)) {
+                k++;
+            }
+            table[q] = k;
         }
     }
 
-    public int search(String txt) {
-        int i, j, N = txt.length(), M = pat.length();
-        for (i = 0, j = 0; i < N && j < M; i++) {
-            j = dfa[txt.charAt(i)][j];
+    public int search(String text) {
+        int matchIdx = 0;
+        for (int i = 0; i < text.length(); i++) {
+            while (matchIdx > 0 && pattern.charAt(matchIdx) != text.charAt(i)) {
+                // Not a match, try to find the next longest prefix
+                matchIdx = table[matchIdx - 1];
+            }
+            if (pattern.charAt(matchIdx) == text.charAt(i)) {
+                matchIdx++;
+            }
+            if (matchIdx == pattern.length()) {
+                return i - pattern.length() + 1;
+            }
         }
-        if (j == M) {
-            return i - M;
-        } else {
-            return N;
-        }
+        // Not found
+        return -1;
     }
 }
